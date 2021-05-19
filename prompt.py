b06d7f7e496943c484f59dfbb09a6c51ff6643b4
LM239 <32392200+LM239@users.noreply.github.com>
2021-05-19 09:00:47 +0200

    Update prompt.py

--- prompt.py
+++ prompt.py
@@ -15,22 +15,25 @@ from Network import LyricSTM as LyricSTM
 
 
 if __name__ == "__main__":
+    #prompts the network and prints the generated results
+    #prompt genre(s), number of prompts per genre, prompt length, and max (generated) lyric length can be set
+    
     minibatch_size = 32
-    model_file = "models/lowlr/model"
-    unique_genres = ["Jazz"]  # sorted(["Rock", "Metal", "Country", "Jazz", "Hip-Hop"])
+    model_file = "models/lowlr/model" # model loaction
+    unique_genres = ["Jazz"] #define genre
+    # sorted(["Rock", "Metal", "Country", "Jazz", "Hip-Hop"])
 
     songs, genres = load_songs(unique_genres)
     alphabet = get_alphabet(songs)
     unique_genres = list(set(genres))
 
-    prompt, prompt_genre = generate_genre_prompts(songs, genres, unique_genres, prompts_per_genre=2)
-    model_ = torch.load("models/newnetonegenre2/model_checkpoint_10")
-    model = LyricSTM(model_.n_hidden, len(alphabet), model_.alphabet, unique_genres)
-    model.load_state_dict(model_.state_dict())
+    #modified prompt parameters are given to generate_gerrnre_prompts()
+    prompt, prompt_genre = generate_genre_prompts(songs, genres, unique_genres, prompts_per_genre=2) # retrieve genre-appropriate prompts
+    model = torch.load("models/newnetonegenre2/model_checkpoint_10") # load model
 
     with torch.no_grad():
         predict = 2000
-        pred_songs = prompt_network(model, prompt, prompt_genre, alphabet, unique_genres, predict=predict, temperature=0.001)
+        pred_songs = prompt_network(model, prompt, prompt_genre, alphabet, unique_genres, predict=predict, temperature=0.001) # generate lyrics
         for i, s in enumerate(pred_songs):
-            print(prompt_genre[i])
+            print(prompt_genre[i]) #print genre and text
             print(prompt[i] + s[len(prompt[i]):])
